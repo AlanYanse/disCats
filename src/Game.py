328ddffc53ee83fe2e0e44f2296f5d8c12bd6e77
AlanYanse <yansealan@gmail.com>
2025-06-21 12:34:17 -0300

    realizando la primera renderización del nivel

--- src/Game.py
+++ src/Game.py
@@ -3,6 +3,7 @@ import pygame
 import sys
 
 from config import *
+from niveles import *
 
 class Game:
 
@@ -13,14 +14,34 @@ class Game:
         self.screen = pygame.display.set_mode((ANCHO, ALTO))
         self.reloj = pygame.time.Clock()
 
+        self.num_tile = {
+            
+            1: None,
+            10: self.obtener_grafico("piso1.png")
+        }
 
+        self.scroll_x = 0
+
+
+
+    
     def update(self):
         pygame.display.flip()
         self.reloj.tick(60) # Para que se refresque a 60 FPS
 
 
     def draw(self):
-        pass
+        # Dibujando el escenario
+        filas = 8
+        columnas = 12
+
+        for y in range(filas):
+            for x in range(columnas):
+                index = y * columnas + x
+                tile = nivel_1_1[index]
+
+                if (tile == 10):
+                    self.screen.blit(self.num_tile[tile][0], (x * 64, y * 64))
 
 
     def check_event(self):
@@ -44,3 +65,15 @@ class Game:
             self.check_event()
             self.update()
             self.draw()
+
+
+
+    def obtener_grafico(self, nombre_archivo):
+        #img = pygame.image.load(f"assets/{nombre_archivo}").convert()
+        img = pygame.image.load(f"assets/{nombre_archivo}").convert()
+        escala_x = 64
+        escala_y = 64
+        image = pygame.transform.scale(img, (escala_x,escala_y)) # Para escalar los tiles
+        image.set_colorkey(BLANCO)
+        rect = image.get_rect() # Hay que obtener el rectangulo para poder interactuar con los tiles
+        return (image, rect)
--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 
 # CONFIGURACIONES DE pantalla
 
-ANCHO, ALTO = 800, 500
+ANCHO, ALTO = 768, 512 # 12 tiles de ancho y 8 de alto
 BLANCO = (255, 255, 255)
